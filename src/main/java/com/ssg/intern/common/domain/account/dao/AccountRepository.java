5e94226449f50bcd4172769f30703f634d9521a0
java-saeng <gusdn85554@naver.com>
2023-01-11 23:07:04 +0900

    :bug: fix : STR-10 JpaRepository 제네릭을 Account로 수정

--- src/main/java/com/ssg/intern/common/domain/account/dao/AccountRepository.java
+++ src/main/java/com/ssg/intern/common/domain/account/dao/AccountRepository.java
@@ -1,6 +1,7 @@
 package com.ssg.intern.common.domain.account.dao;
 
+import com.ssg.intern.common.domain.account.entity.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AccountRepository extends JpaRepository<AccountRepository, Long> {
+public interface AccountRepository extends JpaRepository<Account, Long> {
 }
