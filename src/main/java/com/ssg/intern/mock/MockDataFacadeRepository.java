0a043e5fcb8239879d6b06b251128641f6f25770
java-saeng <gusdn85554@naver.com>
2023-01-11 22:51:13 +0900

    :sparkles: feat : STR-10 mock data를 가져오기 위해 facade repository 생성
    - mock data 조회 시 각 다른 Repository를 주입받고, 조회해야함. 따라서 실제 비즈니스 로직을 행하는 Service에 많은 Repository를 주입받기 때문에 mock data만 조회하는 Facade Repository를 생성하여 하나의 Repository에서 mock data를 조회할 수 있도록 함.

---
+++ src/main/java/com/ssg/intern/mock/MockDataFacadeRepository.java
@@ -0,0 +1,21 @@
+package com.ssg.intern.mock;
+
+import com.ssg.intern.mock.domain.hashtag.dao.HashTagRepository;
+import com.ssg.intern.mock.domain.product.dao.ProductRepository;
+import com.ssg.intern.mock.domain.review.dao.SpecialReviewRepository;
+import com.ssg.intern.mock.domain.review.entity.SpecialReview;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@RequiredArgsConstructor
+@Component
+public class MockDataFacadeRepository {
+
+    private final HashTagRepository hashTagRepository;
+    private final ProductRepository productRepository;
+    private final SpecialReviewRepository specialReviewRepository;
+
+    public SpecialReview findBySpecialReviewId(long specialReviewId) {
+        return specialReviewRepository.findBySpecialReviewId(specialReviewId);
+    }
+}
