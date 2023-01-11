3c479acad01ac2dacb376abc2da3c4ff5ae9d9ec
java-saeng <gusdn85554@naver.com>
2023-01-11 22:49:02 +0900

    :sparkles: feat : STR-10 querydsl를 사용하여 스페셜 리뷰 조회
    - 스페셜 리뷰 조회 시 account, product fetch join

--- src/main/java/com/ssg/intern/mock/domain/review/dao/SpecialReviewRepository.java
+++ src/main/java/com/ssg/intern/mock/domain/review/dao/SpecialReviewRepository.java
@@ -3,5 +3,5 @@ package com.ssg.intern.mock.domain.review.dao;
 import com.ssg.intern.mock.domain.review.entity.SpecialReview;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface SpecialReviewRepository extends JpaRepository<SpecialReview, Long> {
+public interface SpecialReviewRepository extends JpaRepository<SpecialReview, Long>, SpecialReviewRepositoryCustom {
 }
