09251f7f6d3485e067eeee377719e2707128fc3f
java-saeng <gusdn85554@naver.com>
2023-01-11 14:10:40 +0900

    :sparkles: feat : STR-10 mock data 가져오기 위한 JpaRepository 추가
    :sparkles: feat : STR-10 mock data 가져오기 위한 JpaRepository 추가
    
    :sparkles: feat : STR-10 mock data 가져오기 위한 JpaRepository 추가

---
+++ src/main/java/com/ssg/intern/common/domain/account/dao/AccountRepository.java
@@ -0,0 +1,6 @@
+package com.ssg.intern.common.domain.account.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AccountRepository extends JpaRepository<AccountRepository, Long> {
+}
