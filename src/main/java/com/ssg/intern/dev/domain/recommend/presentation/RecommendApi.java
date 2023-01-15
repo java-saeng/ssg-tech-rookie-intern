6a4886c36bedce721449e95807fd41ec3c63ff05
java-saeng <gusdn85554@naver.com>
2023-01-15 18:20:22 +0900

    :sparkles: feat : STR-11 recommend 추가 API 수정
    - recommend는 사용자가 피드 당 한개만 사용가능하므로 굳이 recommendId를 사용하여 식별하지 않고 feed, account 식별자로 recommend 식별

--- src/main/java/com/ssg/intern/dev/domain/recommend/presentation/RecommendApi.java
+++ src/main/java/com/ssg/intern/dev/domain/recommend/presentation/RecommendApi.java
@@ -19,11 +19,10 @@ public class RecommendApi {
 
     private final RecommendCommandService recommendCommandService;
 
-    @PostMapping(value = {"/feeds/{feed-id}/recommends/{recommend-id}", "/feeds/{feed-id}/recommends"})
-    public long plusRecommend(@RequestHeader(HttpHeaders.AUTHORIZATION) long accountId,
-                              @PathVariable("feed-id") long feedId,
-                              @PathVariable(value = "recommend-id", required = false) Optional<Long> recommendId) {
-        return recommendCommandService.addRecommendToFeedByFeedId(accountId, feedId, recommendId);
+    @PostMapping( "/feeds/{feed-id}/recommends")
+    public void plusRecommend(@RequestHeader(HttpHeaders.AUTHORIZATION) long accountId,
+                              @PathVariable("feed-id") long feedId) {
+        recommendCommandService.addRecommendToFeedByFeedId(accountId, feedId);
     }
 
     @DeleteMapping("/feeds/{feed-id}/recommends/{recommend-id}")
