d91c5a1eb7fb5e725e817b2ee598db4b8292c52a
java-saeng <gusdn85554@naver.com>
2023-01-15 21:42:25 +0900

    :sparkles: feat : STR-12 SpecialReview에서 조건에 맞는 SpecialReview 가져오기

--- src/main/java/com/ssg/intern/mock/domain/review/dao/SpecialReviewRepositoryImpl.java
+++ src/main/java/com/ssg/intern/mock/domain/review/dao/SpecialReviewRepositoryImpl.java
@@ -1,9 +1,20 @@
 package com.ssg.intern.mock.domain.review.dao;
 
+import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.ssg.intern.dev.domain.feed.presentation.model.FeedSearchingConditionRequest;
+import com.ssg.intern.mock.domain.review.entity.CookLevel;
+import com.ssg.intern.mock.domain.review.entity.CookQuantity;
+import com.ssg.intern.mock.domain.review.entity.CookTime;
 import com.ssg.intern.mock.domain.hashtag.entity.QHashTag;
 import com.ssg.intern.mock.domain.review.entity.SpecialReview;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.support.PageableExecutionUtils;
+import org.springframework.util.StringUtils;
+
+import java.util.List;
 
 import static com.ssg.intern.common.domain.account.entity.QAccount.account;
 import static com.ssg.intern.mock.domain.hashtag.entity.QHashTag.*;
@@ -26,5 +37,43 @@ public class SpecialReviewRepositoryImpl implements SpecialReviewRepositoryCusto
                            .on(specialReview.id.eq(hashTag.specialReview.id))
                            .where(specialReview.id.eq(specialReviewId))
                            .fetchOne();
+    }
+
+    @Override
+    public Page<SpecialReview> findBySearchingCondition(final Pageable pageable,
+                                                        final FeedSearchingConditionRequest condition) {
+
+        final List<SpecialReview> specialReviews = queryFactory.selectFrom(specialReview).distinct()
+                                                               .innerJoin(hashTag).fetchJoin()
+                                                               .on(specialReview.id.eq(hashTag.specialReview.id))
+                                                               .innerJoin(account).fetchJoin()
+                                                               .on(account.id.eq(specialReview.account.id))
+                                                               .innerJoin(product).fetchJoin()
+                                                               .on(account.id.eq(specialReview.account.id))
+                                                               .where(containsHashtag(condition.getHashTag()))
+                                                               .where(eqCookLevel(condition.getCookLevel()))
+                                                               .where(eqCookQuantity(condition.getCookQuantity()))
+                                                               .where(eqCookTime(condition.getCookTime()))
+                                                               .offset(pageable.getOffset())
+                                                               .limit(pageable.getPageSize())
+                                                               .fetch();
+
+        return PageableExecutionUtils.getPage(specialReviews, pageable, specialReviews::size);
+    }
+
+    private static BooleanExpression eqCookTime(final CookTime cookTime) {
+        return cookTime == null ? null : specialReview.cookTime.eq(cookTime);
+    }
+
+    private static BooleanExpression eqCookQuantity(final CookQuantity cookQuantity) {
+        return cookQuantity == null ? null : specialReview.cookQuantity.eq(cookQuantity);
+    }
+
+    private static BooleanExpression eqCookLevel(final CookLevel cookLevel) {
+        return cookLevel == null ? null : specialReview.cookLevel.eq(cookLevel);
+    }
+
+    private static BooleanExpression containsHashtag(final String keyword) {
+        return StringUtils.hasText(keyword) ? hashTag.name.contains(keyword) : null;
     }
 }
