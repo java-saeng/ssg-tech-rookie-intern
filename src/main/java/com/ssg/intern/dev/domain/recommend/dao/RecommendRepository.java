630b2a7c614cebf6fff6cc7acac2436658481e40
java-saeng <gusdn85554@naver.com>
2023-01-17 09:07:42 +0900

    :sparkles: feat : STR-11 사용하지 않는 쿼리 메서드 삭제

--- src/main/java/com/ssg/intern/dev/domain/recommend/dao/RecommendRepository.java
+++ src/main/java/com/ssg/intern/dev/domain/recommend/dao/RecommendRepository.java
@@ -2,14 +2,7 @@ package com.ssg.intern.dev.domain.recommend.dao;
 
 import com.ssg.intern.dev.domain.recommend.entity.Recommend;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-
-import java.util.Optional;
 
 public interface RecommendRepository extends JpaRepository<Recommend, Long>, RecommendRepositoryCustom {
 
-    @Override
-    @Query("select r from Recommend r inner join fetch r.feed where r.id=:id")
-    Optional<Recommend> findById(@Param("id") Long recommendId);
 }
