f3d06700132c284bcbe15bdb942552154fe55645
xx10222 <yjw4960@gmail.com>
2023-01-30 13:26:24 +0900

    :sparkles: feat : STR-31 해시태그 드롭다운 완료

--- src/main/java/com/ssg/intern/mock/domain/hashtag/dao/HashTagRepository.java
+++ src/main/java/com/ssg/intern/mock/domain/hashtag/dao/HashTagRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface HashTagRepository extends JpaRepository<HashTag, Long> {
-    List<HashTag> findTop5ByOrderByIdAsc();
+    List<HashTag> findDistinctTop10ByOrderByIdAsc();
 }
