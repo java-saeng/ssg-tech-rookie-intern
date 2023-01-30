47cb851ae1bdc0f7af0ab995a54996d413cdc669
xx10222 <yjw4960@gmail.com>
2023-01-30 11:09:02 +0900

    :sparkles: feat : STR-31 기본 해시태그 미리보기 추가

--- src/main/java/com/ssg/intern/mock/domain/hashtag/dao/HashTagRepository.java
+++ src/main/java/com/ssg/intern/mock/domain/hashtag/dao/HashTagRepository.java
@@ -3,5 +3,8 @@ package com.ssg.intern.mock.domain.hashtag.dao;
 import com.ssg.intern.mock.domain.hashtag.entity.HashTag;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface HashTagRepository extends JpaRepository<HashTag, Long> {
+    List<HashTag> findTop5ByOrderByIdAsc();
 }
