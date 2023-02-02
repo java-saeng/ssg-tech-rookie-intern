0f5f1e2c2a41a98e271c0818cbcde09016d7c66e
java-saeng <gusdn85554@naver.com>
2023-02-02 15:48:03 +0900

    :sparkles: feat : STR-33  recommend 저장할 시 buffer를 통해서 저장
    - 테스트를 위해 15초마다 buffer flush하는 스케줄러 추가

--- src/main/java/com/ssg/intern/dev/domain/recommend/service/RecommendCommandService.java
+++ src/main/java/com/ssg/intern/dev/domain/recommend/service/RecommendCommandService.java
@@ -4,19 +4,30 @@ import com.ssg.intern.dev.domain.feed.dao.FeedRepository;
 import com.ssg.intern.dev.domain.feed.entity.Feed;
 import com.ssg.intern.dev.domain.recommend.dao.RecommendRepository;
 import com.ssg.intern.dev.domain.recommend.entity.Recommend;
-import lombok.RequiredArgsConstructor;
+import com.ssg.intern.dev.global.buffer.BufferStatus;
+import com.ssg.intern.dev.global.buffer.ConcurrentMapBuffer;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityNotFoundException;
+import java.util.List;
 
 @Service
-@RequiredArgsConstructor
 @Transactional
 public class RecommendCommandService {
 
     private final RecommendRepository recommendRepository;
     private final FeedRepository feedRepository;
+    private final RecommendQueryService recommendQueryService;
+    private final ConcurrentMapBuffer concurrentMapBuffer;
+
+    public RecommendCommandService(final RecommendRepository recommendRepository, final FeedRepository feedRepository,
+                                   final RecommendQueryService recommendQueryService) {
+        this.recommendRepository = recommendRepository;
+        this.feedRepository = feedRepository;
+        this.recommendQueryService = recommendQueryService;
+        concurrentMapBuffer = new ConcurrentMapBuffer();
+    }
 
     public void addRecommendToFeedByFeedId(final long accountId, final long feedId) {
 
@@ -57,5 +68,33 @@ public class RecommendCommandService {
                                        }
                                    })
                            );
+    }
+
+    public void bufferFlush() {
+
+        final List<BufferStatus> flushCandidate = concurrentMapBuffer.flush();
+
+        for (final BufferStatus bufferStatus : flushCandidate) {
+            if (bufferStatus.isDeleteFlag()) {
+                cancelRecommendToFeedByFeedId(bufferStatus.getAccountId(), bufferStatus.getFeedId());
+            } else {
+                addRecommendToFeedByFeedId(bufferStatus.getAccountId(), bufferStatus.getFeedId());
+            }
+        }
+    }
+
+    public void bufferCaching(final long accountId, final long feedId) {
+
+        if (concurrentMapBuffer.isBufferActivityInBuffer(feedId, accountId)) {
+            concurrentMapBuffer.put(feedId, accountId, false);
+            return;
+        }
+
+        recommendQueryService.findBookmarkByAccountIdAndFeedId(accountId, feedId)
+                            .ifPresentOrElse((recommend) -> {
+                                concurrentMapBuffer.put(feedId, accountId, recommend.isRecommended());
+                            }, () -> {
+                                concurrentMapBuffer.put(feedId, accountId, false);
+                            });
     }
 }
--- src/main/java/com/ssg/intern/dev/global/Scheduler.java
+++ src/main/java/com/ssg/intern/dev/global/Scheduler.java
@@ -1,6 +1,7 @@
 package com.ssg.intern.dev.global;
 
 import com.ssg.intern.dev.domain.bookmark.service.BookmarkCommandService;
+import com.ssg.intern.dev.domain.recommend.service.RecommendCommandService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -14,10 +15,17 @@ import org.springframework.stereotype.Component;
 public class Scheduler {
 
     private final BookmarkCommandService bookmarkCommandService;
+    private final RecommendCommandService recommendCommandService;
 
     @Scheduled(cron = "0/15 * * * * *")
     public void flushBookmarkBuffer() {
         log.info("bookmarkBuffer Flush");
         bookmarkCommandService.bufferFlush();
+    }
+
+    @Scheduled(cron = "0/15 * * * * *")
+    public void flushRecommendBuffer() {
+        log.info("recommendBuffer Flush");
+        recommendCommandService.bufferFlush();
     }
 }
