a0cb098e8eb938d78f21330f6d2ea0400ec72d1b
java-saeng <gusdn85554@naver.com>
2023-02-02 22:16:47 +0900

    :sparkles: feat : STR-39 스페셜 리뷰에서 조회할 때 정렬해서 가져오기

--- src/main/java/com/ssg/intern/mock/domain/review/dao/SpecialReviewRepositoryCustomImpl.java
+++ src/main/java/com/ssg/intern/mock/domain/review/dao/SpecialReviewRepositoryCustomImpl.java
@@ -1,9 +1,12 @@
 package com.ssg.intern.mock.domain.review.dao;
 
+import com.querydsl.core.types.Order;
+import com.querydsl.core.types.OrderSpecifier;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.JPAExpressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.ssg.intern.dev.domain.feed.presentation.model.FeedSearchingConditionRequest;
+import com.ssg.intern.dev.global.SortingCondition;
 import com.ssg.intern.mock.domain.review.entity.CookLevel;
 import com.ssg.intern.mock.domain.review.entity.CookQuantity;
 import com.ssg.intern.mock.domain.review.entity.CookTime;
@@ -11,12 +14,14 @@ import com.ssg.intern.mock.domain.review.entity.SpecialReview;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.support.PageableExecutionUtils;
 import org.springframework.util.StringUtils;
 
 import java.util.List;
 
 import static com.ssg.intern.common.domain.account.entity.QAccount.account;
+import static com.ssg.intern.dev.domain.feed.entity.QFeed.feed;
 import static com.ssg.intern.mock.domain.hashtag.entity.QHashTag.*;
 import static com.ssg.intern.mock.domain.product.entity.QProduct.product;
 import static com.ssg.intern.mock.domain.review.entity.QSpecialReview.specialReview;
@@ -48,10 +53,13 @@ public class SpecialReviewRepositoryCustomImpl implements SpecialReviewRepositor
                                                                .on(account.id.eq(specialReview.account.id))
                                                                .innerJoin(product).fetchJoin()
                                                                .on(product.id.eq(specialReview.product.id))
+                                                               .innerJoin(feed)
+                                                               .on(feed.specialReviewId.eq(specialReview.id))
                                                                .where(eqCookLevel(condition), eqCookQuantity(condition),
                                                                       eqCookTime(condition), hashTagSubQuery(condition))
                                                                .offset(pageable.getOffset())
                                                                .limit(pageable.getPageSize())
+                                                               .orderBy(sort(pageable.getSort()))
                                                                .fetch();
 
         return PageableExecutionUtils.getPage(specialReviews, pageable, specialReviews::size);
@@ -60,7 +68,7 @@ public class SpecialReviewRepositoryCustomImpl implements SpecialReviewRepositor
     private static BooleanExpression hashTagSubQuery(final FeedSearchingConditionRequest condition) {
 
         return StringUtils.hasText(condition.getHashTag()) ? JPAExpressions.selectFrom(hashTag)
-                                                                           .where(hashTag.specialReview.id
+                                                                          .where(hashTag.specialReview.id
                                                                                           .eq(specialReview.id)
                                                                                           .and(containsHashtag(
                                                                                                   condition.getHashTag())))
@@ -81,5 +89,17 @@ public class SpecialReviewRepositoryCustomImpl implements SpecialReviewRepositor
 
     private static BooleanExpression containsHashtag(final String keyword) {
         return StringUtils.hasText(keyword) ? hashTag.name.contains(keyword) : null;
+    }
+
+    private OrderSpecifier<?> sort(final Sort sort) {
+        Order direction = Order.DESC;
+
+        for (final Sort.Order order : sort) {
+            if (order.getProperty().equals(SortingCondition.RECOMMENDATION.toString())) {
+                return new OrderSpecifier<>(direction, feed.recommendCount);
+            }
+        }
+
+        return new OrderSpecifier<>(direction, feed.createdAt);
     }
 }
